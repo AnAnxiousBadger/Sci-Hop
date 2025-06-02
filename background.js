ed610d9e15a2571ea5867321f613c1dd515fede7
Szabolcs Farkas <f.szabi.19@gmail.com>
2025-06-02 20:15:16 +0200

    migrated to manifest V3

--- background.js
+++ background.js
@@ -1,49 +1,74 @@
 // Main extension button - open article on sci-hub
-chrome.browserAction.onClicked.addListener(function() {
-    chrome.tabs.query({
-        currentWindow: true,
-        active: true
-    }, function(tab) {
-		SearchDOI(tab[0].url)
-    });
+chrome.action.onClicked.addListener(function() {
+	sciHopCurrentURL();
 });
-
+// Main extension shortcut - open article on sci-hub
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "sci_hop") {
+	sciHopCurrentURL();
+  }
+});
+// Get current URL and search it on sci-hub
+function sciHopCurrentURL(){
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const currentUrl = tabs[0].url;
+      searchDOI(currentUrl);
+    });
+}
+// Open given DOI on sci-hub
+function searchDOI(DOI){
+	targetURL = encodeURI("https://sci-hub.se/" + DOI);
+	chrome.tabs.create({
+		"url": targetURL
+	});
+}
+/*
 // Right click selected and open article on sci-hub
 chrome.contextMenus.create({
 	title: 'Sci-Hop this → %s',
 	contexts: ['selection'],
-	onclick: SearchSelectedDOI
+	onclick: searchSelectedDOI
 });
 
 // Right click and copy citation
 chrome.contextMenus.create({
 	title: 'Copy Scitation',
 	contexts: ['all'],
-	onclick: CopyCitation
+	onclick: copyCitation
 });
 
 // Listen to found citation message
-chrome.runtime.onMessage.addListener(
+/*chrome.runtime.onMessage.addListener(
 	function (request, sender) {
 		if (request.citationText) {
 			console.log("citation text is: " + request.citationText);
 		}
 	}
-);
+);*/
+/*
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.url && tab.url.includes("sci-hub")) {
+    //chrome.action.setPopup({ tabId: tabId, popup: "popups/on_sci_hub_popup.html" });
+	chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    }, function(tab){
+        chrome.tabs.sendMessage(
+            tab[0].id, {action: "show_on_sci_hub_popup"}
+        );
+    });
+  } else {
+    //chrome.action.setPopup({ tabId: tabId, popup: "" });
+  }
+});
 
 // FUNCTIONS
-function SearchSelectedDOI(info){
-	console.log("clicked on icon");
-	SearchDOI(info.selectionText);
-}
-function SearchDOI(DOI){
-	targetURL = encodeURI("https://sci-hub.se/" + DOI);
-	chrome.tabs.create({
-		"url": targetURL
-	});
-}
+function searchSelectedDOI(info){
+	searchDOI(info.selectionText);
+}*/
 
-function CopyCitation(ocClickData, tab){
+/*
+function copyCitation(ocClickData, tab){
     chrome.tabs.query({
         currentWindow: true,
         active: true
@@ -52,5 +77,5 @@ function CopyCitation(ocClickData, tab){
             tab[0].id, {action: "get_citation"}
         );
     });  
-}
+}*/
 
--- content.js
+++ content.js
@@ -1,19 +1,16 @@
-// Liten to copy citation menu click message
+// Liten to background actions
 chrome.runtime.onMessage.addListener(
 	function (request, sender){
 		if (request.action == "get_citation"){
-            ParseCleanDOI();
-			/*var citation = document.getElementById("citation");
-			if (citation != null) {
-				chrome.runtime.sendMessage({ citationText: ParseAndCopyCitation(citation.innerText) });
-			} else {
-				alert("No citation found");
-			}*/
+            parseCleanDOI();
 		}
+        else if(request.action == "show_on_sci_hub_popup"){
+            injectPopupHTML();
+        }
 	}
 );
 
-function ParseAndCopyCitation(citationText) {
+function parseAndCopyCitation(citationText) {
 
     const yearMatch = citationText.match(/\((\d{4})\)/);
     const year = yearMatch ? yearMatch[1] : '';
@@ -33,19 +30,47 @@ function ParseAndCopyCitation(citationText) {
     });
 }
 
-function ParseCleanDOI(){
+function parseCleanDOI(){
 	const doiDiv = document.getElementById("doi");
     const DOIString = doiDiv.innerText;
-	FetchDOIMetaData(DOIString);
+	fetchDOIMetaData(DOIString);
 }
-function FetchDOIMetaData(DOI){
+function fetchDOIMetaData(DOI){
     const apiURL = `https://api.crossref.org/works/${DOI}`;
     fetch(apiURL).then(response => response.json()).then(data => {
-        const title = data.message.title[0];
+        /*const title = data.message.title[0];
         const year = data.message.issued["date-parts"][0][0];
-        console.log(year);
+        const author = data.message.author[0].family;
+        const journal = data.message["container-title"][0];*/
+
+        const metadata = {
+            year: data.message.issued["date-parts"][0][0],
+            title: data.message.title[0],
+            author: data.message.author[0].family,
+            journal: data.message["container-title"][0],
+            doi: DOI,
+            url: `https://sci-hub.se/${DOI}`
+        };
+        console.log(metadata);
+        return metadata;
     })
 }
-function CopyDataToClipboard(year, title, author){
+function copyDataToClipboard(year, title, author){
 
+}
+
+async function injectPopupHTML() {
+  const url = chrome.runtime.getURL("popups/on_sci_hub_popup.html");
+  const response = await fetch(url);
+  const html = await response.text();
+
+  const container = document.createElement("div");
+  container.id = "popup-container";
+  container.innerHTML = html;
+  document.body.appendChild(container);
+
+  // Load the popup script (e.g. popup/popup.js) if needed
+  /*const script = document.createElement("script");
+  script.src = chrome.runtime.getURL("popup/popup.js");
+  document.body.appendChild(script);*/
 }
